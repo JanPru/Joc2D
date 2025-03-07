d022d878e08fa85e44ae5da0b8aad81005798b01
bernat.biosca <bernat.biosca@estudiantat.upc.edu>
2025-03-07 11:57:37 +0100

    Commit tilemap

---
+++ tilemap.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="tilemap" tilewidth="16" tileheight="16" tilecount="22" columns="11">
+ <image source="../UNI2425/VJ/tiles.png" width="176" height="32"/>
+</tileset>
